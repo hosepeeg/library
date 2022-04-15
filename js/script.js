8b13a1dc40d3f6a7481b2435560974116c6fe006
hosepeeg <hkourbouchian@gmail.com>
2022-04-15 17:57:11 -0400

    setup HTML blueprint

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+let book = {};
+
+function Book(author, title, pages, read) {
+  // the constructor..
+
+  //Object variables
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
+
+  //Object Functions
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  //Add a book to libray
+  book = new Book(auther.value, title.value, pages.value, read.value);
+  myLibrary.push(book);
 }
