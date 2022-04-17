b4cef65e54b3e2f3a4d7592e2b545bcbba8f2f12
hosepeeg <hkourbouchian@gmail.com>
2022-04-17 19:25:39 -0400

    submit button adds book to array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-let myLibrary = [];
+const form = document.querySelector('form');
+const submitButton = form.querySelector('.addBook');
+const title = form.querySelector('#title');
+const author = form.querySelector('#author');
+const pages = form.querySelector('#pages');
+const read = form.querySelector('#read');
 
+let myLibrary = [];
 let book = {};
 
 function Book(author, title, pages, read) {
@@ -14,8 +20,28 @@ function Book(author, title, pages, read) {
   //Object Functions
 }
 
+const addError = (ele) => {
+  // this function will add error text
+}
+
+const removeError = (ele) => {
+  // this function will remove error text
+}
+
+const validateForm = () => {
+  if (title.value == "" && document.querySelector('#titleError') == null) addError(title);
+  if (author.value == "" && document.querySelector('#titleError') == null) addError(author);
+  if (pages.value == "" && document.querySelector('#titleError') == null) addError(pages);
+
+  if (title.value == '' || pages.input == '' || author.input == '') return false;
+  else return true;
+}
+
 function addBookToLibrary() {
   //Add a book to libray
-  book = new Book(auther.value, title.value, pages.value, read.value);
+  if(validateForm() == false) return;
+  book = new Book(author.value, title.value, pages.value, read.value);
   myLibrary.push(book);
 }
+
+submitButton.addEventListener('click', addBookToLibrary);
